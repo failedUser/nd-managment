256334eadec392982b9e23d1b711b44fd4deb4f8
changsu <changsu@caibeike.coom>
2020-08-19 08:38:36 +0800

    feat: 修改域名配置;

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ console.log('--1212-', process.env.NODE_ENV);
 
 let reqUrl = 'http://newdreamer.cn:8080';
 
-if (window.location.href.indexOf('localhost')) {
+if (window.location.href.indexOf('localhost') >= 0) {
     reqUrl = '';   
 }
 

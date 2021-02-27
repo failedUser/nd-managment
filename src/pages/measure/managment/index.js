72c9ec48d0a01f10296227a025157e24fa016924
changsu <changsu@caibeike.coom>
2021-02-27 21:14:58 +0800

    feat: 优化

--- src/pages/measure/managment/index.js
+++ src/pages/measure/managment/index.js
@@ -1,35 +1,35 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './index.less';
 import { Button, Table, Modal, Input, Upload, message, DatePicker, Select } from 'antd';
-import { requestMeasureList,  requestForMeasureEdit, requestForMeasureCreate, requestForMeasureDelete, requestForMeasureExport } from './action';
+import { requestMeasureList, requestForMeasureEdit, requestForMeasureCreate, requestForMeasureDelete, requestForMeasureExport } from './action';
 const { RangePicker } = DatePicker;
 
 export default function ProductManager() {
-    const [ isInit, setIsinit ] = useState(false);
-    const [ pageInfo, updatePageInfo ] = useState({
+    const [isInit, setIsinit] = useState(false);
+    const [pageInfo, updatePageInfo] = useState({
         page: 1,
         size: 10,
         name: '',
         phone: '',
         college: ''
     })
-    const [ tableSize, setTableSize ] = useState(0);
-    const [ dataSource, updateSource ] = useState(null);
-    const [ visible, setVisible ] = useState(false);
-    const [ modalInfo, setModalInfo ] = useState(null);
-    const [ chooseItems, setChooseItems ] = useState(null);
+    const [tableSize, setTableSize] = useState(0);
+    const [dataSource, updateSource] = useState(null);
+    const [visible, setVisible] = useState(false);
+    const [modalInfo, setModalInfo] = useState(null);
+    const [chooseItems, setChooseItems] = useState(null);
 
     const updateSearch = useCallback((key, value) => {
         updatePageInfo(search => {
             search[key] = value;
-            return {...search}
+            return { ...search }
         });
     }, [])
 
     const export_data = useCallback(() => {
         if (!chooseItems || chooseItems.length <= 0) {
             //message.info('请先选择商品, 再导出数据');
-//            return ;
+            //            return ;
         }
 
         requestForMeasureExport(chooseItems)
@@ -40,7 +40,7 @@ export default function ProductManager() {
 
     const edit = useCallback((item) => {
         setVisible('edit');
-        setModalInfo({...item});
+        setModalInfo({ ...item });
     }, []);
 
     const create = useCallback(() => {
@@ -49,15 +49,16 @@ export default function ProductManager() {
     }, []);
 
     const updateModalInfo = useCallback((key, value) => {
-        setModalInfo(info => ({...info, ...{[key]: value}}));
+        setModalInfo(info => ({ ...info, ...{ [key]: value } }));
     }, [])
 
     const pageData = useCallback(() => {
-        let _pageInfo = {...pageInfo};
+        let _pageInfo = { ...pageInfo };
         _pageInfo.page -= 1;
         requestMeasureList(_pageInfo).then(data => {
             setTableSize(data.totalElements)
-            updateSource(data.content)
+            let content = data.content.filter(item => item.volumer_Address !== "1")
+            updateSource(content)
         })
     }, [pageInfo])
 
@@ -65,13 +66,13 @@ export default function ProductManager() {
     const _delete_batch = useCallback(() => {
         if (!chooseItems || chooseItems.length <= 0) {
             message.info('请先选择商品, 再批量删除');
-            return ;
+            return;
         }
         requestForMeasureDelete(chooseItems)
-        .then(() => {
-            message.info('删除成功');
-        })
-        .then(pageData)
+            .then(() => {
+                message.info('删除成功');
+            })
+            .then(pageData)
     }, [chooseItems, pageData])
 
     const submit = useCallback(() => {
@@ -89,11 +90,11 @@ export default function ProductManager() {
                 pageData()
             })
         }
-        
+
     }, [modalInfo, pageData, visible])
 
     const updateStatus = useCallback((record) => {
-        record = {...record};
+        record = { ...record };
         record.volumer_Status = record.volumer_Status === '停用' ? '启用' : '停用';
         requestForMeasureEdit(record).then(res => {
             message.info('修改成功');
@@ -104,7 +105,7 @@ export default function ProductManager() {
     const onPageChange = useCallback((page) => {
         if (page !== pageInfo.page) {
             pageInfo.page = page;
-            updatePageInfo({...pageInfo});
+            updatePageInfo({ ...pageInfo });
             pageData();
         }
     }, [pageData, pageInfo])
@@ -117,24 +118,26 @@ export default function ProductManager() {
     }, [isInit, pageData])
 
 
-    const [ columns ] = useState([
+    const [columns] = useState([
         { title: '姓名', dataIndex: 'volumer_Name' },
-            { title: '电话', dataIndex: 'volumer_Phone'},
-            { title: '性别', dataIndex: 'volumer_Gender'},
-            { title: '身份证号', dataIndex: 'volumer_Address'},
-            // { title: '量体师微信', dataIndex: 'volumer_Wechat_Id'},
-            { title: '出生年月', dataIndex: 'volumer_Birth'},
-            { title: '所属高校', dataIndex: 'volumer_College'},
-            { title: '校区', dataIndex: 'volumer_Part' },
-            { title: '专业', dataIndex: 'volumer_Department'},
-            { title: '状态', dataIndex: 'volumer_Status'},
-            { title: '操作', dataIndex: 'name11', render: (item, record) => <div className="product-table-operations">
-               <Button type="primary" onClick={() => edit(record)} size="small" >修改</Button>
-               <Button type="primary" onClick={() => updateStatus(record)} size="small" >{record.volumer_Status === '停用' ? '启用' : '停用'}</Button>
-            </div>},
-        ])
+        { title: '电话', dataIndex: 'volumer_Phone' },
+        { title: '性别', dataIndex: 'volumer_Gender' },
+        { title: '身份证号', dataIndex: 'volumer_Address' },
+        // { title: '量体师微信', dataIndex: 'volumer_Wechat_Id'},
+        { title: '出生年月', dataIndex: 'volumer_Birth' },
+        { title: '所属高校', dataIndex: 'volumer_College' },
+        { title: '校区', dataIndex: 'volumer_Part' },
+        { title: '专业', dataIndex: 'volumer_Department' },
+        { title: '状态', dataIndex: 'volumer_Status' },
+        {
+            title: '操作', dataIndex: 'name11', render: (item, record) => <div className="product-table-operations">
+                <Button type="primary" onClick={() => edit(record)} size="small" >修改</Button>
+                <Button type="primary" onClick={() => updateStatus(record)} size="small" >{record.volumer_Status === '停用' ? '启用' : '停用'}</Button>
+            </div>
+        },
+    ])
     const createConfig = columns.slice(0, columns.length - 1);
-    createConfig.push({ title: '量体师微信', dataIndex: 'volumer_Wechat_Id'})
+    createConfig.push({ title: '量体师微信', dataIndex: 'volumer_Wechat_Id' })
     return <div className="product-manager">
         <section className="product-manager-search">
             <div className="manager-search-item">
@@ -149,25 +152,25 @@ export default function ProductManager() {
                 <div className="search-item__title">所属高校</div>
                 <Input size="small" placeholder="请输入高校" onChange={e => updateSearch('college', e.target.value)} />
             </div>
-            
+
             <div className="manager-search-btn"><Button onClick={pageData} type="primary" >筛选</Button></div>
         </section>
         <section className="product-manager-operation">
-        {/* <Button onClick={_delete_batch} type="primary">批量删除</Button> */}
+            {/* <Button onClick={_delete_batch} type="primary">批量删除</Button> */}
             <Button onClick={export_data} type="primary">数据导出</Button>
             <Button onClick={create} type="primary">新增</Button>
         </section>
         <section className="product-manager-table">
-            <Table 
+            <Table
                 rowKey="volumer_Id"
                 rowSelection={{
                     type: 'checkbox',
                     onChange: (selectedRowKeys, selectedRows) => {
                         setChooseItems((selectedRowKeys + '').split(',').filter(item => item));
-                      }
+                    }
                 }}
-                dataSource={dataSource} 
-                columns={columns} 
+                dataSource={dataSource}
+                columns={columns}
                 pagination={{
                     current: pageInfo.page,
                     total: tableSize,
@@ -176,13 +179,13 @@ export default function ProductManager() {
             />
         </section>
         {modalInfo && <Modal
-                title="商品编辑"
-                visible={visible}
-                width={1000}
-                onOk={submit}
-                onCancel={() => setVisible(false)}
-            >
-                <div className="pm-edit-container">
+            title="商品编辑"
+            visible={visible}
+            width={1000}
+            onOk={submit}
+            onCancel={() => setVisible(false)}
+        >
+            <div className="pm-edit-container">
                 {createConfig.map(col => <div className="pm-edit-item">
                     <span className="edit-item__title">{col.title}</span>
                     {
@@ -191,10 +194,10 @@ export default function ProductManager() {
                             <Select.Option value="启用">启用</Select.Option>
                         </Select>
                     }
-                    {col.dataIndex !== 'volumer_Status' && <Input value={modalInfo[col.dataIndex]} onChange={e => updateModalInfo(col.dataIndex, e.target.value)} />} 
+                    {col.dataIndex !== 'volumer_Status' && <Input value={modalInfo[col.dataIndex]} onChange={e => updateModalInfo(col.dataIndex, e.target.value)} />}
                 </div>)}
-                </div>
-            </Modal>}
-        
-        </div>
+            </div>
+        </Modal>}
+
+    </div>
 }
--- src/pages/order/evaluate/index.js
+++ src/pages/order/evaluate/index.js
@@ -1,27 +1,27 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './index.less';
 import { Button, Table, Modal, Input, Upload, message, DatePicker, Popover } from 'antd';
 import { requestEvaluateList, requestEvaluateDelete, requestEvaluateExport, requestOrderDetail, } from './action';
-import {exportFile} from '../../../assets/js/common';
+import { exportFile } from '../../../assets/js/common';
 const { RangePicker } = DatePicker;
 
 
 export default function OrderEvaluate() {
-    const [ isInit, setIsinit ] = useState(false);
-    const [ pageInfo, updatePageInfo ] = useState({
+    const [isInit, setIsinit] = useState(false);
+    const [pageInfo, updatePageInfo] = useState({
         page: 0,
         size: 10
     })
-    const [ tableSize, setTableSize ] = useState(0);
-    const [ dataSource, updateSource ] = useState(null);
-    const [ visible, setVisible ] = useState(false);
-    const [ modalInfo, setModalInfo ] = useState(null);
-    const [ chooseItems, setChooseItems ] = useState(null);
+    const [tableSize, setTableSize] = useState(0);
+    const [dataSource, updateSource] = useState(null);
+    const [visible, setVisible] = useState(false);
+    const [modalInfo, setModalInfo] = useState(null);
+    const [chooseItems, setChooseItems] = useState(null);
 
     const updateSearch = useCallback((key, value) => {
         updatePageInfo(info => {
             info[key] = value;
-            return {...info}
+            return { ...info }
         });
     }, [])
 
@@ -29,7 +29,7 @@ export default function OrderEvaluate() {
     const export_data = useCallback(() => {
         if (!chooseItems || chooseItems.length <= 0) {
             //message.info('请先选择商品, 再导出数据');
-//            return ;
+            //            return ;
         }
 
         // TODO 导出怎么传参数
@@ -38,7 +38,7 @@ export default function OrderEvaluate() {
 
     const showOrderVoucher = useCallback((item) => {
         setVisible(true);
-        setModalInfo({...item});
+        setModalInfo({ ...item });
     }, []);
 
     const pageData = useCallback(() => {
@@ -60,66 +60,70 @@ export default function OrderEvaluate() {
     const delete_data = useCallback(() => {
         if (!chooseItems || chooseItems.length <= 0) {
             message.info('先选择要删除的评价');
-            return ;
+            return;
         }
         _delete(chooseItems);
     }, [_delete, chooseItems])
     const onPageChange = useCallback((page) => {
         if (page !== pageInfo.page) {
             pageInfo.page = page;
-            updatePageInfo({...pageInfo});
+            updatePageInfo({ ...pageInfo });
             pageData();
         }
     }, [pageData, pageInfo])
-    
+
     useEffect(() => {
         if (isInit) return;
         pageData();
         setIsinit(true);
     }, [isInit, pageData])
-    const [ ModalColumns ] = useState([
+    const [ModalColumns] = useState([
         { title: '单品编号', dataIndex: 'item_Id' },
-            { title: '商品', dataIndex: 'name'},
-            { title: '条码', dataIndex: 'barcode'},
-            { title: '颜色', dataIndex: 'style'},
-            { title: '尺码', dataIndex: 'size', },
-            { title: '数量', dataIndex: 'amounts'},
-            { title: '单价', dataIndex: 'retail_Price'},
-            { title: '折扣', dataIndex: 'discount'},
-            { title: '折后价', render: (item, record) => <span>{record.received_Amount / record.amounts}</span>},
-            { title: '折后总金额', dataIndex: 'received_Amount'},
-            { title: '状态', dataIndex: 'item_Status'},
-        ])
+        { title: '商品', dataIndex: 'name' },
+        { title: '条码', dataIndex: 'barcode' },
+        { title: '颜色', dataIndex: 'style' },
+        { title: '尺码', dataIndex: 'size', },
+        { title: '数量', dataIndex: 'amounts' },
+        { title: '单价', dataIndex: 'retail_Price' },
+        { title: '折扣', dataIndex: 'discount' },
+        { title: '折后价', render: (item, record) => <span>{record.received_Amount / record.amounts}</span> },
+        { title: '折后总金额', dataIndex: 'received_Amount' },
+        { title: '状态', dataIndex: 'item_Status' },
+    ])
 
-    const [ columns ] = useState([
-        { title: '单品编号', dataIndex: 'item_Id'},
-        { title: '订单号', dataIndex: 'order_Id'},
-        { title: '商品条码', dataIndex: 'productMain'},
-        { title: '评价时间', dataIndex: 'evaluation_Time'},
-        { title: '微信id', dataIndex: 'customer_Wechat_Id',width: 100},
-        { title: '尺寸', dataIndex: 'size'},
-        { title: '面料', dataIndex: 'fabric'},
-        { title: '做工', dataIndex: 'work',},
+    const [columns] = useState([
+        { title: '单品编号', dataIndex: 'item_Id' },
+        { title: '订单号', dataIndex: 'order_Id' },
+        { title: '商品条码', dataIndex: 'productMain' },
+        { title: '评价时间', dataIndex: 'evaluation_Time' },
+        // { title: '微信id', dataIndex: 'customer_Wechat_Id',width: 100},
+        { title: '尺寸', dataIndex: 'size' },
+        { title: '面料', dataIndex: 'fabric' },
+        { title: '做工', dataIndex: 'work', },
         { title: '物流', dataIndex: 'shipment' },
-        { title: '星级', dataIndex: 'star'},
-        { title: '评价内容', width:200, dataIndex: 'evaluation_Content', render: (text, record) => <Popover trigger="hover" content={text} title="Title">
-           {text.substr(0, 10)}
-        </Popover>,},
-        { title: '操作', dataIndex: 'name11', width: 150, render: (item, record) => <div className="product-table-operations">
-           <Button onClick={() => {
-                requestOrderDetail({orderId:record.order_Id}).then(data => {
-                    setVisible(true);
-                    setModalInfo(data);
-                })
+        { title: '星级', dataIndex: 'star' },
+        {
+            title: '评价内容', width: 200, dataIndex: 'evaluation_Content', render: (text, record) => <Popover trigger="hover" content={text} title="Title">
+                {text.substr(0, 10)}
+            </Popover>,
+        },
+        {
+            title: '操作', dataIndex: 'name11', width: 150, render: (item, record) => <div className="product-table-operations">
+                <Button onClick={() => {
+                    requestOrderDetail({ orderId: record.order_Id }).then(data => {
+                        setVisible(true);
+                        setModalInfo(data);
+                    })
 
-           }} type="primary" size="small" >订单</Button>
-           <Button onClick={() => {
-               _delete([record.evaluation_Id])
-           }} type="primary" size="small" >删除</Button>
-        </div>},
-        ])
-        // TODO 评价没有删除 和订单功能，导出 删除都没有
-   
+                }} type="primary" size="small" >订单</Button>
+                <Button onClick={() => {
+                    _delete([record.evaluation_Id])
+                }} type="primary" size="small" >删除</Button>
+            </div>
+        },
+    ])
+    // TODO 评价没有删除 和订单功能，导出 删除都没有
+
     return <div className="product-manager">
         <section className="product-manager-search">
             <div className="manager-search-item">
@@ -133,7 +137,7 @@ export default function OrderEvaluate() {
                     updateSearch('endTime', dateString[1]);
                 }} />
             </div>
-            
+
             <div className="manager-search-btn"><Button onClick={pageData} type="primary" >筛选</Button></div>
         </section>
         <section className="product-manager-operation">
@@ -141,39 +145,39 @@ export default function OrderEvaluate() {
             <Button onClick={delete_data} type="primary">批量删除</Button>
         </section>
         <section className="product-manager-table">
-            <Table 
+            <Table
                 rowKey="evaluation_Id"
                 rowSelection={{
                     type: 'checkbox',
                     onChange: (selectedRowKeys, selectedRows) => {
                         setChooseItems((selectedRowKeys + '').split(',').filter(item => item));
-                      }
+                    }
                 }}
-                dataSource={dataSource} 
-                columns={columns} 
+                dataSource={dataSource}
+                columns={columns}
                 pagination={{
-                    current: pageInfo.page+1,
+                    current: pageInfo.page + 1,
                     total: tableSize,
                     onChange: onPageChange
                 }}
             />
         </section>
         {modalInfo && <Modal
-                title="单品信息"
-                visible={visible}
-                width={1000}
-                onOk={() => setVisible(false)}
-                onCancel={() => setVisible(false)}
-            >
-                <div className="pm-edit-container">
-                    <Table 
-                        rowKey="order_Id"
-                        dataSource={modalInfo} 
-                        columns={ModalColumns} 
-                        pagination={false}
-                    />
-                </div>
-            </Modal>}
-        
-        </div>
+            title="单品信息"
+            visible={visible}
+            width={1000}
+            onOk={() => setVisible(false)}
+            onCancel={() => setVisible(false)}
+        >
+            <div className="pm-edit-container">
+                <Table
+                    rowKey="order_Id"
+                    dataSource={modalInfo}
+                    columns={ModalColumns}
+                    pagination={false}
+                />
+            </div>
+        </Modal>}
+
+    </div>
 }
